20cbd1aac09b69759c98425e8ff5bdb96887f6e8
Grass-Grower <164552274+Grass-Grower@users.noreply.github.com>
2024-04-18 16:13:26 -0400

    Update main.js

--- static/assets/scripts/main.js
+++ static/assets/scripts/main.js
@@ -1,2 +1,5 @@
 document.getElementById('tab-title') = 'Monkey Moment';
 document.getElementById('favicon') = '../media/favicon/monkey.png';
+
+document.getElementById('header').innerHTML = 'Monkey Moment';
+document.getElementById('main').innerHTML = 'Website information here';
