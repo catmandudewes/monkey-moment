6503b260033b19a1d57aaa546bb37781a69d70b1
wes <131189910+catmandudewes@users.noreply.github.com>
2024-06-03 07:16:28 -0400

    Delete static directory

--- static/assets/scripts/main.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('tab-title') = 'Monkey Moment';
-document.getElementById('favicon') = '../media/favicon/monkey.png';
-
-document.getElementById('header').innerHTML = 'Monkey Moment';
-document.getElementById('main').innerHTML = 'Website information here';
